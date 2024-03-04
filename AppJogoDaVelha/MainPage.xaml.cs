d6fae05249f1d8a9f0c5d0e29a24028a5b791777
Usuario <Usuario@CPD03-M03>
2024-03-04 15:05:30 -0300

    Avanço

--- AppJogoDaVelha/MainPage.xaml.cs
+++ AppJogoDaVelha/MainPage.xaml.cs
@@ -3,12 +3,30 @@ namespace AppJogoDaVelha
 {
     public partial class MainPage : ContentPage
     {
-
+        string vez = "O";
         public MainPage()
         {
             InitializeComponent();
         }
 
+        private void btn10_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Button disparador = (Button)sender;
+
+            disparador.IsEnabled = false;
+
+            if(vez == "O")
+            {
+                disparador.Text = "0";
+                vez = "X";
+            } 
+            else
+            {
+                disparador.Text = "X";
+                vez == "O";
+            }
+
+        }
     }  
 
 }
